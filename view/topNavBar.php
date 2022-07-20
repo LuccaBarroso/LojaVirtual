b4464f3630e9c33cc69053b51f6617f70e959111
Lucca <barrosolucca@gmail.com>
2022-07-20 15:21:52 -0300

    initial commit

--- view/topNavBar.php
+++ view/topNavBar.php
@@ -5,7 +5,7 @@
     <div class="container d-flex flex-wrap">
         <ul class="nav me-auto">
             
-            <li class="nav-item" data-toggle="tooltip" data-placement="top" title="Login"><i class="person-fill"></i></li>
+            <li class="nav-item" data-toggle="tooltip" data-placement="top" title="Login"><i class="bi-person-fill"></i></li>
             <li class="nav-item">teste</li>
         </ul>
     </div>
