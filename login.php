338a2eae7775495417f0559401e1ba6a39a3d8cb
Lucca <barrosolucca@gmail.com>
2022-07-21 11:28:34 -0300

    page title

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+    $title="Login";
     require("model/database.php");
     // require("model/userAuth.php");
     include_once("./view/base/top.php") 
--- register.php
+++ register.php
@@ -1,4 +1,5 @@
 <?php
+    $title="Register";
     require("model/database.php");
     // require("model/userAuth.php");
     include_once("./view/base/top.php") 
